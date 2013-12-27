9fe132d69e628e1e0c909ef9267eb46be1902ad5
Michael Herman <hermanmu@gmail.com>
2013-12-27 14:30:45 -0600

    refactor

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
   google: {
-    returnURL: process.env.URL || 'http://127.0.0.1:1337/auth/google/callback',
+  	var URL = "http://referfriend.herokuapp.com/"
+    returnURL: process.env.URL + "auth/google/callback" || 'http://127.0.0.1:1337/auth/google/callback',
     realm: process.env.URL || 'http://127.0.0.1:1337'
   },
   mongoUrl: process.env.MONGOLAB_URI || 'mongodb://localhost/referFriend'

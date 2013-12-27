8bac0016d819b0e9a4972ad1510d2eaa9c254f2f
Michael Herman <hermanmu@gmail.com>
2013-12-27 14:34:25 -0600

    refactor

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
-var URL = "http://referfriend.herokuapp.com/"
+var herokuReturn = "http://referfriend.herokuapp.com/auth/google/callback"
+var herokuRealm = "http://referfriend.herokuapp.com/"
+
 
 module.exports = {
   google: {
-    returnURL: process.env.URL + "auth/google/callback" || 'http://127.0.0.1:1337/auth/google/callback',
-    realm: process.env.URL || 'http://127.0.0.1:1337'
+    returnURL: process.env.herokuReturn || 'http://127.0.0.1:1337/auth/google/callback',
+    realm: process.env.herokuRealm || 'http://127.0.0.1:1337'
   },
   mongoUrl: process.env.MONGOLAB_URI || 'mongodb://localhost/referFriend'
 };

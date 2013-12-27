89d5587117f28fc17392ec900e2d3790a692e012
Michael Herman <hermanmu@gmail.com>
2013-12-27 12:29:33 -0600

    refactor

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   google: {
-    returnURL: 'http://127.0.0.1:1337/auth/google/callback',
-    realm: 'http://127.0.0.1:1337'
+    returnURL: 'hhttp://referfriend.herokuapp.com/auth/google/callback',
+    realm: 'http://referfriend.herokuapp.com/'
   },
   mongoUrl: process.env.MONGOLAB_URI || 'mongodb://localhost/referFriend'
 };

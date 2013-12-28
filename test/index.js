05e0437f280fa2859bd2927841003221fe38e1f7
Michael Herman <hermanmu@gmail.com>
2013-12-28 12:46:23 -0600

    updates

--- test/index.js
+++ test/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var app = require('../app');
 var request = require('supertest');
 
@@ -6,16 +8,33 @@ describe('referFriend', function(){
   describe('when requesting resource /', function(){
     it('should respond with 200', function(done){
       request(app)
-        .get('/')
-        .expect(200, done);
+      .get('/')
+      .expect(200, done);
     })
   });
+
+  describe('when requesting resource /accounts', function(){
+    it('should respond with 302 - redirect', function(done){
+      request(app)
+      .get('/account')
+      .expect(302, done);
+    })
+  });
+
+  describe('when posting to /account', function(){
+    it('should respond with 200', function(done){
+      request(app)
+      .post('/account')
+      .send({ name: "Mike", email: "user@gluck.com", createdDate:new Date(), refererEmail: "test@tester123.com" })
+      .expect(302, done)        
+    })
+  }); 
 
   describe('when requesting resource /missing', function(){
     it('should respond with 404', function(done){
       request(app)
-        .get('/missing')
-        .expect(404, done);
+      .get('/missing')
+      .expect(404, done);
     })
   });
 
